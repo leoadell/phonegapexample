9f03118145062adbb42f76b1ac87c11272da99ea
leoadell <leoadell@gmail.com>
2013-05-19 00:12:31 -0300

    changed to one page application

--- js/main.js
+++ js/main.js
@@ -1,4 +1,16 @@
 var app = {
+
+	renderHomeView: function() {
+		var html =
+				"<div class='header'><h1>Home</h1></div>" +
+				"<div class='search-view'>" +
+				"<input class='search-key'/>" +
+				"<ul class='employee-list'></ul>" +
+				"</div>"
+		$('body').html(html);
+		$('.search-key').on('keyup', $.proxy(this.findByName, this));
+	},
+
 	showAlert: function (message, title) {
 		if (navigator.notification) {
 			navigator.notification.alert(message, null, title, 'OK');
@@ -24,6 +36,7 @@ var app = {
     var self = this;
     this.store = new WebSqlStore(function() {
         self.showAlert('Store Initialized', 'Info');
+		self.renderHomeView();
     });
     $('.search-key').on('keyup', $.proxy(this.findByName, this));
 }
