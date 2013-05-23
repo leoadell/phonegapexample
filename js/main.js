0220f36a39339f73048ac5cad9e1ff3854d818d1
leoadell <leoadell@gmail.com>
2013-05-23 14:49:37 -0300

    ui style

--- js/main.js
+++ js/main.js
@@ -1,7 +1,21 @@
 var app = {
+	route: function() {
+		var hash = window.location.hash;
+		if (!hash) {
+			$('body').html(new HomeView(this.store).render().el);
+			return;
+		}
+		var match = hash.match(app.detailsURL);
+		if (match) {
+			this.store.findById(Number(match[1]), function(employee) {
+				$('body').html(new EmployeeView(employee).render().el);
+			});
+		}
+	},
 
 	registerEvents: function() {
 		var self = this;
+		$(window).on('hashchange', $.proxy(this.route, this));
 		// Check of browser supports touch events...
 		if (document.documentElement.hasOwnProperty('ontouchstart')) {
 			// ... if yes: register touch event listener to change the "selected" state of the item
@@ -32,11 +46,11 @@ var app = {
 
 	initialize: function() {
 		var self = this;
+		this.detailsURL = /^#employees\/(\d{1,})/;
 		this.registerEvents();
 		this.store = new WebSqlStore(function() {
-		    self.showAlert('Store Initialized', 'Info');
-			$('body').html(new HomeView(self.store).render().el);
-		});	
+			self.route();
+		});
 	}
 
 };
